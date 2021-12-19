0bb50a27c68f3998590966e90a4844185409c4b1
Benjamin Akponasa <baka_1993@yahoo.co.uk>
2021-12-19 23:44:02 +0000

    Create second migration file and add description attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_19_233409) do
+ActiveRecord::Schema.define(version: 2021_12_19_234126) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
   end
 
 end
