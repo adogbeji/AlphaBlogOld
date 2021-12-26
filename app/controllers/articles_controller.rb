5af4372ebde420e0fdf8efcd4aaa10e5bfe1be08
Benjamin Akponasa <baka_1993@yahoo.co.uk>
2021-12-26 21:26:26 +0000

    Add comments to articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,9 @@
 class ArticlesController < ApplicationController
     def show
-        @article = Article.find(params[:id])
+        @article = Article.find(params[:id])  # Finds article based on ID
     end
 
     def index
-        @articles = Article.all
+        @articles = Article.all  # Saves all articles in database table
     end
 end
